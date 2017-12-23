d3842d098d87e21b8232a0c030183353d9055b2f
Solonarv <nstamm@gmx.de>
2017-12-23 17:41:22 +0100

    Solved day 1, part 2.

--- Day1/Main.hs
+++ Day1/Main.hs
@@ -1,17 +1,34 @@
 {-# LANGUAGE DataKinds, FlexibleInstances, TypeFamilies #-}
 module Day1.Main where
 
+import Control.Arrow ((&&&))
+import qualified Data.Vector as V
+
 import Harness
 
 instance Solution 1 where
     type Input 1 = [Int]
-    type Output 1 = Int
+    type Output 1 = (Int, Int)
     readInput = simpleReadInput $ map (read . (:[])) . head . lines
-    process _ = sumDigits
+    process _ = sumNext &&& sumOpposite
+    displayOutput _ (nextSum, oppositeSum) = do
+        putStr "Sum of digits that match the next digit:"
+        print nextSum
+        putStr "Sum of digits that match the opposite digit:"
+        print oppositeSum
 
-sumDigits :: [Int] -> Int
-sumDigits [] = 0
-sumDigits ds@(h:_) = go h 0 ds
+sumNext :: [Int] -> Int
+sumNext [] = 0
+sumNext ds@(h:_) = go h 0 ds
   where 
     go hd acc (x:y:xs) = if x == y then go hd (acc + x) (y:xs) else go hd acc (y:xs)
     go hd acc (x:[]) = if hd == x then (acc + x) else acc
+
+sumOpposite :: [Int] -> Int
+sumOpposite xs = V.sum $ V.zipWith addIfEqual hi lo
+  where
+    vec = V.fromList xs
+    (lo, hi) = V.splitAt (V.length vec `div` 2) vec
+    addIfEqual x y
+        | x == y    = x + y
+        | otherwise = 0
